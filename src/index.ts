6374a5f4ba9263665cb63ab5877e171b55a3690b
Marcus Schölzel <marcus.schoelzel@gmx.de>
2021-07-13 13:45:39 +0200

    Fixed: Some regions where missing. List should be complete for now. Fixed: Error handling when switching regions not possible due to network problems. Fixed: Error handling when authentication is not successfull. Fixed: Message in case of no org. Fixed: Message in case of no space.

--- src/index.ts
+++ src/index.ts
@@ -19,25 +19,46 @@ try {
     const btpRegionCode = (await Enquirer.prompt<{ selection: string }>({
         type: 'select',
         name: 'selection',
-        message: 'Choose BTP region',
-        initial: 3,
+        // https://help.sap.com/viewer/65de2977205c403bbc107264b8eccf4b/LATEST/en-US/350356d1dc314d3199dca15bd2ab9b0e.html
+        message: 'Choose BTP Cloud Foundry region',
+        initial: 7,
         choices:
             [
-                { "name": "ap10", "message": "Australia (Sydney)", "hint": "AWS" },
-                { "name": "br10", "message": "Brazil (Sao Paulo)", "hint": "AWS" },
-                { "name": "ca10", "message": "Canada (Montreal)", "hint": "AWS" },
-                { "name": "eu10", "message": "EU (Frankfurt)", "hint": "AWS" },
-                { "name": "eu20", "message": "EU (Netherlands)", "hint": "Azure" },
-                { "name": "jp10", "message": "Japan (Tokyo)", "hint": "AWS" },
-                { "name": "jp20", "message": "Japan (Tokyo)", "hint": "Azure" },
-                { "name": "ap11", "message": "Singapore", "hint": "AWS" },
-                { "name": "ap21", "message": "Singapore", "hint": "Azure" },
-                { "name": "ap12", "message": "South Korea (Seoul)", "hint": "AWS" },
-                { "name": "us10", "message": "US East (Virginia)", "hint": "AWS" },
-                { "name": "us21", "message": "US East (Virginia)", "hint": "Azure" },
-                { "name": "us20", "message": "US West (Washington)", "hint": "Azure" },
+                { "name": "ap11", "message": "ap11 - Asia Pacific (Singapore)", "hint": "Amazon Web Services" },
+                { "name": "ap12", "message": "ap12 - Asia Pacific (Seoul) ", "hint": "Amazon Web Services" },
+                { "name": "ap10", "message": "ap10 - Australia (Sydney)", "hint": "Amazon Web Services" },
+                { "name": "ap20", "message": "ap20 - Australia (Sydney)", "hint": "Microsoft Azure" },
+                { "name": "br10", "message": "br10 - Brazil (São Paulo)", "hint": "Amazon Web Services" },
+                { "name": "ca10", "message": "ca10 - Canada (Montreal)", "hint": "Amazon Web Services" },
+                { "name": "cn40", "message": "cn40 - China (Shanghai)", "hint": "Alibaba Cloud" },
+                { "name": "eu10", "message": "eu10 - Europe (Frankfurt)", "hint": "Amazon Web Services" },
+                { "name": "eu11", "message": "eu11 - Europe (Frankfurt)", "hint": "Amazon Web Services" },
+                { "name": "eu20", "message": "eu20 - Europe (Netherlands)", "hint": "Microsoft Azure" },
+                { "name": "jp10", "message": "jp10 - Japan (Tokyo)", "hint": "Amazon Web Services" },
+                { "name": "jp20", "message": "jp20 - Japan (Tokyo)", "hint": "Microsoft Azure" },
+                { "name": "ap21", "message": "ap21 - Singapore", "hint": "Microsoft Azure" },
+                { "name": "us10", "message": "us10 - US East (VA)", "hint": "Amazon Web Services" },
+                { "name": "us21", "message": "us21 - US East (VA)", "hint": "Microsoft Azure" },
+                { "name": "us20", "message": "us20 - US West (WA)", "hint": "Microsoft Azure" },
+                { "name": "us30", "message": "us30 - US Central (IA)", "hint": "Google Cloud Platform" }
             ]
     })).selection
+    const btpRegionDomain = btpRegionCode + ((btpRegionCode === 'cn40') ? '.platform.sapcloud.cn' : '.hana.ondemand.com')
+
+    const apiProgress = new clui.Spinner('Switching region, please wait...')
+    apiProgress.start()
+    try {
+        await exec('cf api api.cf.' + btpRegionDomain)
+    } catch (error) {
+        if (error.stderr) {
+            throw error.stderr
+        } else {
+            throw error
+        }
+    } finally {
+        apiProgress.stop()
+    }
+    apiProgress.stop()
 
     const config = new Configstore('btpcflogin')
     if (config.size === 0) {
@@ -59,43 +80,72 @@ try {
         message: 'Choose login user',
         choices: config.get('passLogins')
     })).selection
-
     const btpCredentials = spawnSync('pass', ['show', passEntry], { stdio: ['inherit', 'pipe', 'pipe'] }).stdout.toString().split("\n")
 
     const authProgress = new clui.Spinner('Authenticating you, please wait...')
     authProgress.start()
-    await exec('cf api api.cf.' + btpRegionCode + '.hana.ondemand.com')
-    await exec('cf auth \"' + btpCredentials[1].slice(10) + '\" \"' + btpCredentials[0] + '\"')
+    try {
+        await exec('cf auth \"' + btpCredentials[1].slice(10) + '\" \"' + btpCredentials[0] + '\"')
+    } catch (error) {
+        let errorObj = JSON.parse(error.stderr);
+        if (errorObj.error === 'invalid_grant') {
+            throw errorObj.error_description
+        } else {
+            throw error
+        }
+    } finally {
+        authProgress.stop()
+    }
     authProgress.stop()
 
-    const cfOrgs = (await exec('cf orgs')).stdout.split("\n").splice(3)
-    const cfOrg = (await Enquirer.prompt<{ selection: string }>({
-        type: 'select',
-        name: 'selection',
-        message: 'Choose organisation',
-        choices: cfOrgs
-    })).selection
+    let cfOrgs = (await exec('cf orgs')).stdout.split("\n")
+    if (cfOrgs[2] === 'No orgs found.') {
+        console.log(chalk.yellowBright(cfOrgs[2]))
+    } else {
+        cfOrgs = cfOrgs.splice(3, cfOrgs.length - 4)
 
-    const orgProgress = new clui.Spinner('Switching organisation, please wait...')
-    orgProgress.start()
-    await exec('cf target -o \"' + cfOrg + '\"')
-    orgProgress.stop()
+        const cfOrg = (await Enquirer.prompt<{ selection: string }>({
+            type: 'select',
+            name: 'selection',
+            message: 'Choose organisation',
+            choices: cfOrgs
+        })).selection
 
-    const cfSpaces = (await exec('cf spaces')).stdout.split("\n").splice(3)
-    const cfSpace = (await Enquirer.prompt<{ selection: string }>({
-        type: 'select',
-        name: 'selection',
-        message: 'Choose space',
-        choices: cfSpaces
-    })).selection
+        const orgProgress = new clui.Spinner('Switching organisation, please wait...')
+        orgProgress.start()
+        try {
+            await exec('cf target -o \"' + cfOrg + '\"')
+        } finally {
+            orgProgress.stop()
+        }
+        orgProgress.stop()
 
-    const spaceProgress = new clui.Spinner('Switching space, please wait...')
-    spaceProgress.start()
-    await exec('cf target -s \"' + cfSpace + '\"')
-    spaceProgress.stop()
+        let cfSpaces = (await exec('cf spaces')).stdout.split("\n")
+        if (cfSpaces[2] === 'No spaces found.') {
+            console.log(chalk.yellowBright(cfSpaces[2]))
+        } else {
+            cfSpaces = cfSpaces.splice(3, cfSpaces.length - 4)
+
+            const cfSpace = (await Enquirer.prompt<{ selection: string }>({
+                type: 'select',
+                name: 'selection',
+                message: 'Choose space',
+                choices: cfSpaces
+            })).selection
+
+            const spaceProgress = new clui.Spinner('Switching space, please wait...')
+            spaceProgress.start()
+            try {
+                await exec('cf target -s \"' + cfSpace + '\"')
+            } finally {
+                spaceProgress.stop()
+            }
+            spaceProgress.stop()
+        }
+    }
 
     console.log(chalk.cyanBright((await exec('cf t')).stdout))
 
 } catch (error) {
-    console.error(error)
+    console.error(chalk.redBright(error))
 }

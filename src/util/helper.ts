be7079573763e071e3973e57272c3570d4b900f2
Ludwig Stockbauer-Muhr <stocki87@gmail.com>
2023-12-28 19:00:46 +0100

    feat: print errors if required executables are not on the PATH

--- src/util/helper.ts
+++ src/util/helper.ts
@@ -1,7 +1,20 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import which from "which";
 
 /**
  * Returns directory name from file url (e.g. import.meta.url)
  */
 export const getDirname = (fileUrl: string) => dirname(fileURLToPath(fileUrl));
+
+/**
+ * Tests if the given executable is on the PATH
+ * @param cmd name of an executable on the current PATH
+ */
+export async function assertCmdInPath(cmd: string, cmdLabel?: string): Promise<void> {
+  try {
+    await which(cmd);
+  } catch (error) {
+    throw new Error(`Required executable '${cmdLabel ?? cmd}' could not be found on the PATH`);
+  }
+}
